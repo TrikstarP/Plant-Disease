f8f290dd0630fb6dde61bf174110fdb48620be75
TrikstarP <99407740+TrikstarP@users.noreply.github.com>
2023-06-09 19:05:34 +0800

    Create Models.py

---
+++ Models.py
@@ -0,0 +1,173 @@
+import tensorflow as tf
+from tensorflow.keras import models, layers
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+import sys
+
+
+
+image_wh = 256                          #Dimension both width and height
+BATCH_SIZE = 32                         #Training sample per one iteration
+
+
+
+
+# 80% -> training
+# 20% -> 10% validation, 10% test
+channels = 3
+epochs = 50
+
+
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "Potatoes",
+    shuffle = True,
+    image_size = (image_wh,image_wh),
+    batch_size = BATCH_SIZE
+)
+class_names = dataset.class_names
+class_names
+len(dataset)
+plt.figure(figsize=(10, 10))
+for image_batch, label_batch in dataset.take(1):
+    for i in range (12):
+        ax = plt.subplot(3,4,i+1)
+        plt.imshow(image_batch[i].numpy().astype("uint8"))
+        plt.title(class_names[label_batch[i]])
+        plt.axis("off")
+train_size = 0.8
+len(dataset)*train_size
+train_ds = dataset.take(54)
+len(train_ds)
+test_ds = dataset.skip(54)
+len(test_ds)
+val_size = 0.1
+len(dataset)*val_size
+val_ds = test_ds.take(6)
+len(val_ds)
+test_ds = test_ds.skip(6)
+len(test_ds)
+def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    ds_size = len(ds)
+    
+    if shuffle:
+        ds = ds.shuffle (shuffle_size, seed = 12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)
+    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    return train_ds, val_ds, test_ds
+train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
+len(train_ds), len(val_ds), len(test_ds)
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+resize_and_rescale = tf.keras.Sequential([
+    layers.experimental.preprocessing.Resizing(image_wh, image_wh),
+    layers.experimental.preprocessing.Rescaling(1.0/255)
+])
+data_augmentation = tf.keras.Sequential([
+    layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+    layers.experimental.preprocessing.RandomRotation(0.2),
+])
+input_shape = (BATCH_SIZE, image_wh, image_wh, channels)
+n_classes = 3
+
+model = models.Sequential([
+    resize_and_rescale,
+    data_augmentation,
+    layers.Conv2D(32, (3,3), activation ='relu', input_shape = input_shape),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, kernel_size = (3,3), activation ='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, kernel_size = (3,3), activation ='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation ='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation ='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation ='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation ='relu'),
+    layers.Dense(n_classes, activation ='softmax'),
+])
+model.build(input_shape=input_shape)
+model.summary()
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy']
+)
+history = model.fit(
+    train_ds,
+    epochs=epochs,
+    batch_size=BATCH_SIZE,
+    verbose=1,
+    validation_data=val_ds
+)
+scores = model.evaluate(test_ds)
+scores
+history.params
+history.history.keys()
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(range(epochs), acc, label='Training Accuray')
+plt.plot(range(epochs), val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(range(epochs), loss, label='Training Loss')
+plt.plot(range(epochs), val_loss, label='Validation Loss')
+plt.legend(loc='lower right')
+plt.title('Training and validation Accuracy')
+plt.show()
+np.argmax([5.1160738e-02, 9.4883752e-01, 1.6446759e-06])
+for images_batch, labels_batch in test_ds.take(1):
+    
+    first_image = images_batch[0].numpy().astype('uint8')
+    first_label = labels_batch[0].numpy()
+    print("The Image to Predict")
+    
+    batch_prediction = model.predict(images_batch, verbose=0)
+    print("Prediction            :", class_names[np.argmax(batch_prediction[0])])
+    
+    
+    plt.imshow(first_image)
+    print("The Image actual label:", class_names[first_label])
+def predict(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(images[i].numpy())
+    img_array = tf.expand_dims(img_array, 0)
+    
+    prediction = model.predict(img_array)
+    
+    predicted_class = class_names[np.argmax(prediction[0])]
+    confidence = round(100*(np.max(prediction[0])), 2)
+    return predicted_class, confidence
+# Temporarily redirect stdout to a null device
+sys.stdout = open('nul', 'w')  # For Windows
+
+plt.figure(figsize=(15, 15))
+for images, labels in test_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3 , i+1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        
+        predicted_class, confidence = predict(model, images[i].numpy())
+        actual_class = class_names[labels[i]]
+        
+        plt.title(f"Prediction : {predicted_class}, \n Actual class : {actual_class}.")
+        
+        plt.axis('off')
+model_version = 2
+model.save('D:\Personal\Project\Plant Disease\Models\model_version_{}'.format(model_version))
